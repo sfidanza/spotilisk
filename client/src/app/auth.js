872a5054634be1e943d38d77b7a2e30f893b3d27
Stephane Fidanza <stephane.fidanza@gmail.com>
2024-08-08 18:31:42 +0200

    fix: trigger refreshToken when accessToken is expired

---
+++ client/src/app/auth.js
@@ -0,0 +1,120 @@
+import { AUTH_CLIENT_ID, AUTH_REDIRECT_URL } from './config.js';
+
+const authorizationEndpoint = 'https://accounts.spotify.com/authorize';
+const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+const scope = 'user-read-private user-read-email user-library-read';
+
+export const currentToken = {
+    get access_token() { return localStorage.getItem('access_token') || null; },
+    get refresh_token() { return localStorage.getItem('refresh_token') || null; },
+    get expires_in() { return localStorage.getItem('refresh_in') || null },
+    get expires() { return localStorage.getItem('expires') || null },
+
+    save: function (response) {
+        const { access_token, refresh_token, expires_in } = response;
+        localStorage.setItem('access_token', access_token);
+        localStorage.setItem('refresh_token', refresh_token);
+        localStorage.setItem('expires_in', expires_in);
+
+        const now = new Date();
+        const expiry = new Date(now.getTime() + (expires_in * 1000));
+        localStorage.setItem('expires', expiry);
+    }
+};
+
+export async function redirectToAuthCodeFlow() {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem('verifier', verifier);
+
+    const authUrl = new URL(authorizationEndpoint);
+    authUrl.search = new URLSearchParams({
+        'client_id': AUTH_CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': AUTH_REDIRECT_URL,
+        'scope': scope,
+        'code_challenge_method': 'S256',
+        'code_challenge': challenge
+    }).toString();
+
+    document.location = authUrl.toString();
+}
+
+function generateCodeVerifier(length) {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const randomValues = crypto.getRandomValues(new Uint8Array(length));
+    const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], '');
+
+    return randomString;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode(...new Uint8Array(digest)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+export async function getToken(code) {
+    const verifier = localStorage.getItem('verifier');
+
+    const response = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({
+            'client_id': AUTH_CLIENT_ID,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': AUTH_REDIRECT_URL,
+            'code_verifier': verifier
+        })
+    });
+    const token = await response.json();
+    if (response.ok) {
+        currentToken.save(token);
+        checkTokenExpiry();
+    } else {
+        console.error('getToken failed. Check logs.');
+    }
+
+    return token;
+}
+
+export async function refreshToken() {
+    const response = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({
+            client_id: AUTH_CLIENT_ID,
+            grant_type: 'refresh_token',
+            refresh_token: currentToken.refresh_token
+        }),
+    });
+    const token = await response.json();
+    if (response.ok) {
+        currentToken.save(token);
+        checkTokenExpiry();
+    } else {
+        // TODO: handle case where refresh token is expired
+        console.error('refreshToken failed. Clear localStorage and reload page to sign in again.');
+    }
+
+    return token;
+}
+
+let rtTimeoutId = null;
+
+export async function checkTokenExpiry() {
+    const now = new Date();
+    const expireDelay = new Date(currentToken.expires).getTime() - now.getTime();
+    if (expireDelay < 60 * 1000) { // if less than 1mn remaining, refresh now
+        await refreshToken();
+    } else {
+        if (rtTimeoutId) clearTimeout(rtTimeoutId);
+        rtTimeoutId = setTimeout(refreshToken, expireDelay - 60 * 1000);
+        console.log(`refreshToken planned in ${expireDelay / 1000 - 60} seconds.`);
+    }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,6 @@
-import * as login from './app/login.js';
-import { AUTH_CLIENT_ID, AUTH_REDIRECT_URL } from './app/config.js';
+import * as auth from './app/auth.js';
 
 const SPOTIFY_API_BASE = 'https://api.spotify.com/v1';
-
-const currentToken = {
-    get access_token() { return localStorage.getItem('access_token') || null; },
-    get refresh_token() { return localStorage.getItem('refresh_token') || null; },
-    get expires_in() { return localStorage.getItem('refresh_in') || null },
-    get expires() { return localStorage.getItem('expires') || null },
-
-    save: function (response) {
-        const { access_token, refresh_token, expires_in } = response;
-        localStorage.setItem('access_token', access_token);
-        localStorage.setItem('refresh_token', refresh_token);
-        localStorage.setItem('expires_in', expires_in);
-
-        const now = new Date();
-        const expiry = new Date(now.getTime() + (expires_in * 1000));
-        localStorage.setItem('expires', expiry);
-    }
-};
 
 // On page load, try to fetch auth code from current browser search URL
 const params = new URLSearchParams(window.location.search);
@@ -27,19 +8,17 @@ const code = params.get('code');
 
 // If we find a code, we're in a callback, do a token exchange
 if (code) {
-    const token = await login.getToken(code, AUTH_CLIENT_ID, AUTH_REDIRECT_URL);
-    currentToken.save(token);
+    await auth.getToken(code);
 
     // Remove code from URL so we can refresh correctly.
     const url = new URL(window.location.href);
     url.searchParams.delete('code');
-
-    const updatedUrl = url.search ? url.href : url.href.replace('?', '');
-    window.history.replaceState({}, document.title, updatedUrl);
+    window.history.replaceState({}, document.title, url.toString());
 }
 
 // If we have a token, we're logged in, so fetch user data and render logged in template
-if (currentToken.access_token) {
+if (auth.currentToken.access_token) {
+    await auth.checkTokenExpiry();
     const userData = await getSpotifyData('/me');
     console.log(userData);
     const playlists = await getSpotifyData('/me/playlists');
@@ -47,19 +26,23 @@ if (currentToken.access_token) {
     const tracks = await getSpotifyData('/me/tracks');
     console.log(tracks);
     // renderTemplate('main', 'logged-in-template', userData);
-    // renderTemplate('oauth', 'oauth-template', currentToken);
+    // renderTemplate('oauth', 'oauth-template', auth.currentToken);
 }
 
 // Otherwise we're not logged in, so render the login template
-if (!currentToken.access_token) {
+if (!auth.currentToken.access_token) {
     // renderTemplate('main', 'login');
-    login.redirectToAuthCodeFlow(AUTH_CLIENT_ID, AUTH_REDIRECT_URL);
+    auth.redirectToAuthCodeFlow();
 }
 
 async function getSpotifyData(endpoint) {
-    const result = await fetch(SPOTIFY_API_BASE + endpoint, {
+    const response = await fetch(SPOTIFY_API_BASE + endpoint, {
         method: 'GET',
-        headers: { Authorization: `Bearer ${currentToken.access_token}` }
+        headers: { Authorization: `Bearer ${auth.currentToken.access_token}` }
     });
-    return await result.json();
+    const result = await response.json();
+    if (result.error) {
+        console.error(result.error.message);
+    }
+    return result;
 }
