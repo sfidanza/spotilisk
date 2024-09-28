sfidanza/spotilisk

commit 35523a0369d7db5838898aed84b31a431600d27d

feat: Preview is now shown on Playlist Tracks
-Preview was in console till now

client/src/app/page.js
@@ -131,14 +131,18 @@ page.getTrackDiff = function (playlistId) {
 		console.error('Playlist has not been selected, so its tracks have not been retrieved yet');
 	}
 	const trackDiff = page.data.tracks.items.filter((li) => tracks.every(pi => pi.track.id !== li.track.id));
-	return trackDiff;
+	return [trackDiff, tracks];
 };
 
 page.previewTrackDiff = function () {
 	const playlistId = page.data.selected;
 	if (playlistId) {
-		const toBeAdded = page.getTrackDiff(playlistId);
+		const [toBeAdded, originalTracks] = page.getTrackDiff(playlistId);
 		console.log(toBeAdded.map(it => it.track.name));
+
+		page.templates.tracks.parse(toBeAdded);
+		page.templates.tracks.parse(originalTracks);
+		page.templates.tracks.load('tracks');
 	}
 };
 

client/src/templates/main.html
@@ -1,16 +1,16 @@
 <!-- BEGIN: liked -->
 <h3>Liked songs ({tracks.total})</h3>
 <div class="expand" data-onclick="toggleLikedSongs">Preview / See all</div>
-<div class="liked preview">
-    <!-- BEGIN: track -->
-    <img src="{track.img}" title="{track.name} - {track.artist}"/>
-    <!-- END: track -->
-</div>
-<!-- END: liked -->
+    <div class="liked preview">
+        <!-- BEGIN: track -->
+        <img src="{track.img}" title="{track.name} - {track.artist}" />
+        <!-- END: track -->
+    </div>
+    <!-- END: liked -->
 
 <div class="sync">
     <button onclick="page.syncIntoPlaylist()">Sync Liked Songs into the playlist selected below</button>
-    <button onclick="page.previewTrackDiff()">Preview in console</button>
+    <button onclick="page.previewTrackDiff()">Preview</button>
 </div>
 
 <h3>Playlists</h3>

commit 38ebd5576406edf87100273330654188a4cb87dd

fix: sync was broken by commit 35523a0

client/src/app/page.js
@@ -150,7 +150,7 @@ page.previewTrackDiff = function () {
 page.syncIntoPlaylist = function () {
 	const playlistId = page.data.selected;
 	if (playlistId) {
-		const toBeAdded = page.getTrackDiff(playlistId);
+		const [ toBeAdded ] = page.getTrackDiff(playlistId);
 		if (toBeAdded.length) {
 			spotify.addTracksToPlaylist(playlistId, toBeAdded.map(it => it.track.uri));
 		}